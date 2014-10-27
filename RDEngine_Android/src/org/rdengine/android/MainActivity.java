bb8b6adf273a98a24f5c353175003df856873873
Rainer.Yang <yangyu@mofang.com>
2014-10-27 16:59:36 +0800

    new commit

---
+++ RDEngine_Android/src/org/rdengine/android/MainActivity.java
@@ -0,0 +1,16 @@
+package org.rdengine.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
